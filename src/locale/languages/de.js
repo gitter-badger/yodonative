93e58c7fd09b50d108dd1c4f96fb6b19422aab6d
Florian Taut <florian@taut-fl.de>
2017-08-25 09:47:48 +0200

    initial commit
    first initial commit

---
+++ src/locale/languages/de.js
@@ -0,0 +1,10 @@
+export const de = {
+    welcome: {
+        app: "Yodo Native",
+        url: "https://github.com/yoders/yodonative",
+        title: "Willkommen bei Yodo Native!",
+
+
+        start: "Los gehts!"
+    }
+}
